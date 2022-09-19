5a05a4e76ffca9e8e33e7542dab7d811d12a07bc
Carla Gonzalez <oryza.acc@gmail.com>
2022-09-19 00:36:00 -0300

    BaSP-W04: ex01-variables and operators

---
+++ Semana-04/exercises/01_variables_and_operators.js
@@ -0,0 +1,20 @@
+/* A  */
+
+var number1 = 4
+var number2 = 6
+var number3 = number1 + number2
+console.log(number3)
+
+/* B  */
+
+var stri1 = 'Carla Gonzalez '
+var stri2 = 'is fabolous'
+var stri3 = stri1 + stri2
+console.log(stri3)
+
+/* C */
+
+var stri4 = 'asdf'
+var stri5 = 'qwerty'
+var stri6 = stri4.length + stri5.length
+console.log(stri6)
